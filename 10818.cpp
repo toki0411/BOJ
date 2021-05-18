b20b1e4943e9f13bc9dc6c8d95cfca42a43b38cc
신혜진 <toki402@naver.com>
2021-05-19 00:58:01 +0900

    최소, 최대

---
+++ 10818.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#pragma warning(disable:4996)
+
+using namespace std;
+int main() {
+
+    int a, b;
+    int arr[100001] = { 0, };
+    cin >> a;
+
+    for (int i = 0; i < a; i++) {
+        cin >> b;
+        arr[i] = b;
+    }
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < a; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+    cout << min << "\n" << max;
+}
