36ce083d2177542f94485937480457df6ff70640
신혜진 <toki402@naver.com>
2021-07-24 21:53:52 +0900

    A+B-2

---
+++ 2558.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+}
