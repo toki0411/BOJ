65cb7660c926ad5b0d6b49e43ab76336b3ce554e
신혜진 <toki402@naver.com>
2021-08-09 01:12:46 +0900

    좌표 정렬하기 2

---
+++ 11651.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector <pair<int,int>> xy;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int N;
+	cin >> N;
+	int a,b;
+	for (int i = 0; i < N;i++) {
+		cin >> a;
+		cin >> b;
+		xy.push_back(make_pair(b, a));
+		
+	}
+	sort(xy.begin(), xy.end());
+	
+	for (int i = 0; i < N; i++) {
+		cout << xy[i].second<<' '<<xy[i].first <<"\n";
+	}
+}
