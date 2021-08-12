a6bf6db544d40c077d41ad32b7799b1b48df8ee9
신혜진 <toki402@naver.com>
2021-08-12 19:23:23 +0900

    최소 힙

---
+++ 1927.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <functional>
+using namespace std; 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	priority_queue <int> q;
+	int N;
+	cin >> N;
+	int a = 0;
+	while (N--) {
+		cin >> a;
+		if (a == 0) {
+			if (q.empty())
+				cout << '*'<<0 << endl;
+			else {
+				cout <<'*'<< -q.top();
+				q.pop();
+
+			}
+		}
+		else {
+			q.push(-a);
+		}
+	}
+}
