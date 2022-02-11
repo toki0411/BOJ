f3aa259f8c95197db748533bd34581201b6b2efb
신혜진 <toki402@naver.com>
2022-02-11 19:57:20 +0900

    N과 M (1)

---
+++ 15649.cpp
@@ -0,0 +1,36 @@
+//
+//  Copyright (c) 2021 HyeJin Shin All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+int N,M;
+int arr[9];
+bool visited[9];
+using namespace std;
+void dfs(int cnt){
+    if(cnt==M){
+        for(int i=0;i<M;i++){
+            cout<<arr[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    for(int i=1;i<=N;i++){
+        if(!visited[i]){
+            visited[i]=true;
+            arr[cnt]=i;
+            dfs(cnt+1);
+            visited[i]=false;  
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> N>>M;
+    dfs(0);
+}
+
