7fa00f3a5cc78db204054cd25d41df1c2abc3baf
toki0411 <toki402@naver.com>
2023-08-11 16:07:47 +0900

    가장 긴 바이토닉 부분 수

---
+++ 11722.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1] * 1001
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[j]+1, dp[i])
+print(max(dp))
+
+
+
