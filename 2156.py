a41bcfc1d0b8ad47ea0e9f0b5593901ac0e86bb3
toki0411 <toki402@naver.com>
2023-08-15 16:06:32 +0900

    포도주 시식

---
+++ 2156.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [0] * 10000
+for i in range(n):
+    arr[i]=int(input())
+
+dp = [0] * 10000
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+dp[2] = max(arr[2] + arr[0], arr[2]+arr[1], dp[1])
+
+for i in range(3,n):
+    dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i], dp[i-1])
+print(max(dp))
