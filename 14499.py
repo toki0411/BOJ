d142283a1bf4a63e249e6a203b4e717ffddab10b
toki0411 <toki402@naver.com>
2023-09-12 00:01:42 +0900

    주사위 굴리기

---
+++ 14499.py
@@ -0,0 +1,40 @@
+n, m, x, y, k = map(int, input().split())
+
+board = []
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+dice = [0, 0, 0, 0, 0, 0]
+
+
+def turn(dir):
+    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if dir == 1:  # 동
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+    elif dir == 2:  # 서
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+    elif dir == 3:  # 북
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
+    else:  # 남
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+
+
+for i in range(n):
+    board.append(list(map(int, input().split())))
+move = list(map(int, input().split()))
+
+nx, ny = x, y
+for i in move:
+    nx += dx[i - 1]
+    ny += dy[i - 1]
+
+    if nx >= n or nx < 0 or ny < 0 or ny >= m:
+        nx -= dx[i - 1]
+        ny -= dy[i - 1]
+        continue
+    turn(i)
+    if board[nx][ny] == 0:
+        board[nx][ny] = dice[-1]
+    else:
+        dice[-1] = board[nx][ny]
+        board[nx][ny] = 0
+    print(dice[0])
